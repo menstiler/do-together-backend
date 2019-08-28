666a7f05a096a3546df86816d406ce2a347f2a75
Mendel Stillerman <menstiler@gmail.com>
2019-08-28 18:16:30 -0400

    hide passwords from all endpoints

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,4 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :title, :location, :events, :groups, :users, :image, :icon
+  has_many :users, include_nested_associations: true
 end
